4bdbc501a3a25312ec157d84d39dd7e4c5124f5f
charleslai <laihongchang@xd.com>
2025-01-07 21:33:07 +0800

    merge 5.6.3

--- labelu/internal/statics/frontend_version.js
+++ labelu/internal/statics/frontend_version.js
@@ -1,15 +1,16 @@
 (function () {
   window.__frontend = {
-    version: '5.5.1',
+    version: '5.6.2',
     deps: {
-      '@labelu/audio-annotator-react': '1.6.0',
-      '@labelu/components-react': '1.6.0',
+      '@labelu/audio-annotator-react': '1.7.4',
+      '@labelu/components-react': '1.7.4',
       '@labelu/formatter': '1.0.2',
-      '@labelu/image': '1.2.1',
-      '@labelu/image-annotator-react': '2.2.1',
+      '@labelu/i18n': '1.0.2',
+      '@labelu/image': '1.3.0',
+      '@labelu/image-annotator-react': '2.3.2',
       '@labelu/interface': '1.3.1',
-      '@labelu/video-annotator-react': '1.3.6',
-      '@labelu/video-react': '1.3.5',
+      '@labelu/video-annotator-react': '1.4.2',
+      '@labelu/video-react': '1.4.2',
     },
   };
 })();
