1affe5e3a26cc78d5c33c247fbf999a1bdc151de
charleslai <laihongchang@xd.com>
2025-03-15 13:25:55 +0800

    fix mysql pswd

--- labelu/internal/common/config.py
+++ labelu/internal/common/config.py
@@ -52,7 +52,7 @@ class Settings(BaseSettings):
 
     def get_mysql_url(self) -> str:
         """Build MySQL URL from individual components if needed"""
-        encoded_password = urllib.parse.quote(self.MYSQL_PASSWORD, safe='')  # safe='' 代表所有特殊字符都编码
+        encoded_password = urllib.parse.quote_plus(self.MYSQL_PASSWORD, safe='')  # safe='' 代表所有特殊字符都编码
         database_url = f"mysql://{self.MYSQL_USER}:{encoded_password}@{self.MYSQL_HOST}:{self.MYSQL_PORT}/{self.MYSQL_DATABASE}"
         database_url = database_url.replace("%", "%%")
         return database_url
