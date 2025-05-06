bc9b67044c4c18c2d2a0665041707533697cef35
charleslai <laihongchang@xd.com>
2025-05-06 20:34:37 +0800

    update frontend to 5.8.11

--- labelu/internal/statics/frontend_version.js
+++ labelu/internal/statics/frontend_version.js
@@ -1,16 +1,16 @@
 (function () {
   window.__frontend = {
-    version: '5.8.5',
+    version: '5.8.8',
     deps: {
-      '@labelu/audio-annotator-react': '1.8.1',
-      '@labelu/components-react': '1.7.9',
+      '@labelu/audio-annotator-react': '1.8.3',
+      '@labelu/components-react': '1.7.10',
       '@labelu/formatter': '1.0.2',
       '@labelu/i18n': '1.0.6',
       '@labelu/image': '1.4.0',
-      '@labelu/image-annotator-react': '2.4.2',
+      '@labelu/image-annotator-react': '2.4.3',
       '@labelu/interface': '1.3.1',
-      '@labelu/video-annotator-react': '1.4.7',
-      '@labelu/video-react': '1.5.1',
+      '@labelu/video-annotator-react': '1.4.9',
+      '@labelu/video-react': '1.5.2',
     },
   };
 })();
